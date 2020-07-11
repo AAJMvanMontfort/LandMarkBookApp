31e3606ba1dd9100d5462b2620c29595500d1c4e
AAJMvanMontfort <lout@me.com>
2020-07-11 10:22:10 +0200

    Initial Commit

---
+++ LandMarkBookApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LandMarkBookApp
+//
+//  Created by AAJM van Montfort on 11/07/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
